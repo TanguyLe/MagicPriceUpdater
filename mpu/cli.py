7373fe6171d42cbd81fa00cfddf8608029bf1198
tanguy <lefloch.ta@gmail.com>
2020-12-03 00:39:17 +0100

    support for dynamic strategies and help plus bumpversion

--- mpu/cli.py
+++ mpu/cli.py
@@ -1,17 +1,33 @@
+import enum
 from pathlib import Path
 
 import os
+import sys
 import typer
 
 from mpu.getstock import main
 
+# Extensions
+sys.path.append(str(Path(__file__).parent / "MPUStrategies"))
+from mpu_strategies.compute_current_price import CurrentPriceComputer
+from mpu_strategies.price_update import PriceUpdater
+
 app = typer.Typer()
+
+CurrentPriceStrat = enum.Enum(
+    'CurrentPriceStrat',
+    {strat: strat for strat in CurrentPriceComputer.get_available_strategies()}
+)
+PriceUpdaterStrat = enum.Enum(
+    'PriceUpdaterStrat',
+    {strat: strat for strat in PriceUpdater.get_available_strategies()}
+)
 
 
 @app.command()
 def getstock(
-        current_price_strategy: str = typer.Argument(...),
-        price_update_strategy: str = typer.Argument(...),
+        current_price_strategy: CurrentPriceStrat,
+        price_update_strategy: PriceUpdaterStrat,
         market_extract_path: Path = typer.Option(
             os.getcwd(),
             exists=True,
@@ -35,8 +51,8 @@ def getstock(
         force_download: bool = typer.Option(False, help="Force download the market extract.")
 ):
     main(
-        current_price_strategy=current_price_strategy,
-        price_update_strategy=price_update_strategy,
+        current_price_strategy=current_price_strategy.value,
+        price_update_strategy=price_update_strategy.value,
         market_extract_path=market_extract_path,
         output_path=output_path,
         force_update=force_download
